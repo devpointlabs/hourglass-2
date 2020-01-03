e14c982455a32b03ab08397dc9c082b13ea67a86
Tristan Johnson <tristan.t.johnson@gmail.com>
2020-01-03 15:57:35 -0700

    added basic controllers

---
+++ test/controllers/api/sessions_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class Api::SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get api_sessions_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get api_sessions_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get api_sessions_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get api_sessions_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get api_sessions_destroy_url
+    assert_response :success
+  end
+
+end
