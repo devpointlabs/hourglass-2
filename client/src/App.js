b66b33cc5acbc66252862433b242c79de9f78400
Ben Ruiz <bneruiz@me.com>
2020-01-06 14:44:59 -0700

    added protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment, } from 'react';
 import { Route, Switch, } from 'react-router-dom';
 import { Container, } from 'semantic-ui-react';
-import SideBar from './components/SideBar';
+import NavBar from './components/NavBar';
 import NoMatch from './components/NoMatch';
 import Login from './components/Login';
 import Register from './components/Register';
@@ -9,6 +9,7 @@ import Home from './components/Home';
 import Tasks from './components/Tasks';
 import Session from './components/Session';
 import Project from './components/Project';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => (
   <Fragment>
@@ -17,10 +18,10 @@ const App = () => (
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-        <Route exact path='/' component={Home} />
-        <Route exact path='/tasks' component={Tasks} />
-        <Route exact path='/project' component={Project} />
-        <Route exact path='/session' component={Session} />
+        <ProtectedRoute exact path='/' component={Home} />
+        <ProtectedRoute exact path='/tasks' component={Tasks} />
+        <ProtectedRoute exact path='/project' component={Project} />
+        <ProtectedRoute exact path='/session' component={Session} />
         <Route component={NoMatch} />
       </Switch>
     </Container>
