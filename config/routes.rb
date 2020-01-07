574352791b1b2f0b9e91505e72689a5506e9e509
Tristan Johnson <tristan.t.johnson@gmail.com>
2020-01-07 12:42:15 -0700

    added projects and admins controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 	namespace :api do
 		resources :users, only: [:index]
 		resources :projects, except: [:new] do
+			resources :project_admins, only: [:index, :update]
 			resources :tasks, except: [:new] do
 			end
 		end
