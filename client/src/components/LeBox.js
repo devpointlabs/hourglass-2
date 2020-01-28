40b710c76b49d148c39e8407cc42cd7e19eeffca
Adam Venord <AdamVenord7@gmail.com>
2020-01-28 15:37:48 -0700

    Lebox update

--- client/src/components/LeBox.js
+++ client/src/components/LeBox.js
@@ -1,11 +1,43 @@
-import React from 'react'
-import { Segment } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Button, Divider, Segment, Transition, Icon } from 'semantic-ui-react'
 
-const LeBox = () => 
+export default class LeBox extends Component {
+  state = { visible: true }
 
-  <Segment compact>    
-  {/* put the clock in here */}
-    Compact content
-  </Segment>
+  toggleVisibility = () =>
+    this.setState((prevState) => ({ visible: !prevState.visible }))
 
-export default LeBox
+  render() {
+    const { visible } = this.state
+
+    return (
+      <>
+        <Icon 
+          size="large"
+          style={{ 
+            position: "fixed", 
+            bottom: 1, 
+            right: 1, 
+            transform: "translateX(-40%) translateY(-700%) "
+          }} 
+          name="stopwatch"
+          // content={visible ? 'Hide' : 'Show'}
+          onClick={this.toggleVisibility}
+        />
+        <Divider hidden />
+        <Transition visible={visible} duration={500}>
+          <Segment compact style={{ 
+            position: "fixed", 
+            bottom: 0, 
+            right: 0, 
+            transform: "translateY(-145%)" 
+          }}>    
+            {/* put the clock in here */}
+              Dude
+          </Segment>
+        </Transition>
+      </>
+    )
+  }
+}
+
