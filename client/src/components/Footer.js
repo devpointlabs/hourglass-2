90e40705038b0a12f4b8b9bfe03b4b3099e90271
Adam Venord <AdamVenord7@gmail.com>
2020-02-21 17:22:48 -0700

    slight footer changes and additions

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,22 +1,33 @@
 import React from 'react'
-import {Container, List, Segment,} from 'semantic-ui-react'
+import {Container, List, Segment, Image} from 'semantic-ui-react'
+import DevImage from './Images/Dev.jpg'
 
 const Footer = () => (
-  <div style={{ position: 'fixed', bottom: '0', width: '100%'}} >
+  <div style={{ position: 'fixed', bottom: '0', width: '100%', color: "white", }} >
     <Segment inverted vertical style={{ background: '#643cc9', margin: '2em 0em 0em', padding: '2em 0em',}}>
     <Container textAlign='center'>
+      {/* <Image src={DevImage} style={{ width: 315, height: 70, }}/> */}
       <List horizontal inverted divided link size='small'>
         <List.Item as='a' >
            {/*put the email or something here  */}
         </List.Item>
-        <List.Item as='a' >
-          Contact Us
+        <List.Item style={{ color: "white" }} >
+        © 2020 DevPoint Labs 
         </List.Item>
-        <List.Item as='a' href='#'>
-          Terms and Conditions
+        <List.Item style={{ color: "white" }} >
+        contact@devpointlabs.com
         </List.Item>
-        <List.Item as='a' href='#'>
-          Privacy Policy
+        <List.Item style={{ color: "white" }}>
+        801-448-7240
+        </List.Item>
+        <List.Item style={{ color: "white" }}>
+        370 S. 300 E. SLC, Utah 84111
+        </List.Item>
+        <List.Item as='a' style={{ color: "white" }}>
+        Terms
+        </List.Item>
+        <List.Item as='a' style={{ color: "white" }}>
+        Policy
         </List.Item>
       </List>
     </Container>
